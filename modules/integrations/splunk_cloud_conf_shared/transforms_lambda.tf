8d62a3b76968677bd0df16276b2e22843a1950fe
Suzuki <dr.smart710@gmail.com>
2025-06-16 16:27:59 +0200

    fix: remove wrong extraction in extra lambdas (#47)

--- modules/integrations/splunk_cloud_conf_shared/transforms_lambda.tf
+++ modules/integrations/splunk_cloud_conf_shared/transforms_lambda.tf
@@ -3,7 +3,7 @@ resource "splunk_configs_conf" "forgecicd_extra_lambda_tenant_fields" {
 
   variables = {
     "REGEX"      = "(?<aws_region>[^:]+):\\/aws\\/lambda\\/(?<forgecicd_tenant>[a-z0-9]+)-(?<forgecicd_region_alias>[a-z0-9]+)-(?<forgecicd_vpc_alias>[a-z0-9]+)-(?<forgecicd_log_type>github-app-runner-group|github-clean-global-lock)"
-    "FORMAT"     = "aws_region::$1 forgecicd_tenant::$2 forgecicd_region_alias::$3 forgecicd_vpc_alias::$4 forgecicd_log_type::$5 forgecicd_type::ec2"
+    "FORMAT"     = "aws_region::$1 forgecicd_tenant::$2 forgecicd_region_alias::$3 forgecicd_vpc_alias::$4 forgecicd_log_type::$5"
     "SOURCE_KEY" = "source"
     "CLEAN_KEYS" = "0"
   }
