476166e82084ff0cd1101b9edf5ed6aa833f7560
Suzuki <dr.smart710@gmail.com>
2025-08-27 15:03:44 +0200

    fix: use individual kube config for each tenant (#99)

--- modules/core/arc/karpenter.tf
+++ modules/core/arc/karpenter.tf
@@ -2,14 +2,20 @@ locals {
   node_pool_manifest = templatefile("${path.module}/templates/node_pool.yaml.tpl", {
     tenant = var.controller_config.namespace
   })
+
+  kubeconfig_path = "${path.cwd}/.kube/${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region}-${var.controller_config.namespace}.kubeconfig"
 }
+
 data "external" "update_kubeconfig" {
   program = ["bash", "-c", <<EOT
+    mkdir -p "$(dirname '${local.kubeconfig_path}')"
+
     aws eks update-kubeconfig \
       --region '${var.aws_region}' \
       --name '${var.eks_cluster_name}' \
       --alias '${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region}' \
-      --profile '${var.aws_profile}' >/dev/null 2>&1
+      --profile '${var.aws_profile}' \
+      --kubeconfig '${local.kubeconfig_path}' >/dev/null 2>&1
 
     echo '{"updated":"true"}'
   EOT
@@ -23,7 +29,7 @@ data "external" "karpenter_ec2nodeclass" {
     "bash",
     "-c",
     <<-EOT
-      kubectl --context ${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region} get ec2nodeclasses.karpenter.k8s.aws karpenter -o yaml \
+      kubectl --kubeconfig '${local.kubeconfig_path}' --context ${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region} get ec2nodeclasses.karpenter.k8s.aws karpenter -o yaml \
       | yq eval '
           del(
             .metadata.creationTimestamp,
@@ -50,11 +56,12 @@ data "external" "karpenter_ec2nodeclass" {
 resource "null_resource" "apply_ec2_node_class" {
   provisioner "local-exec" {
     command = <<EOF
+export KUBECONFIG='${local.kubeconfig_path}'
 # Delete existing EC2NodeClass if it exists (needed to update immutable fields)
 while kubectl --context ${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region} get ec2nodeclasses.karpenter.k8s.aws "karpenter-${var.controller_config.namespace}" >/dev/null 2>&1; do
+  sleep 1
   kubectl --context ${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region} patch ec2nodeclasses.karpenter.k8s.aws "karpenter-${var.controller_config.namespace}" --type=merge -p '{"metadata":{"finalizers":[]}}' || true
   kubectl --context ${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region} delete ec2nodeclasses.karpenter.k8s.aws "karpenter-${var.controller_config.namespace}" --ignore-not-found || true
-  sleep 1
 done
 
 # Apply the new EC2NodeClass manifest only if not migrating
@@ -79,6 +86,7 @@ EOF
 resource "null_resource" "apply_node_pool" {
   provisioner "local-exec" {
     command = <<EOF
+export KUBECONFIG='${local.kubeconfig_path}'
 if [ "${var.migrate_arc_cluster}" = "false" ]; then
   echo "${local.node_pool_manifest}" \
     | kubectl --context ${var.eks_cluster_name}-${var.aws_profile}-${var.aws_region} apply -f -
