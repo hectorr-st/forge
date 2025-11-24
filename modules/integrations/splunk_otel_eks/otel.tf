3079f798db57930ba715073ff4de735a693c5f91
Suzuki <dr.smart710@gmail.com>
2025-11-24 14:45:08 +0100

    chore(deps): update infrastructure dependencies to latest versions (#197)
    * chore(deps): update helm release splunk-otel-collector to v0.140.0 (#14)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update terraform-aws-github-runner version to v6.9.1 (#15)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update terraform terraform-aws-modules/eks/aws to v21.9.0 (#10)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- modules/integrations/splunk_otel_eks/otel.tf
+++ modules/integrations/splunk_otel_eks/otel.tf
@@ -2,7 +2,7 @@ resource "helm_release" "splunk_otel_collector" {
   name             = "splunk-otel-collector"
   repository       = "https://signalfx.github.io/splunk-otel-collector-chart"
   chart            = "splunk-otel-collector"
-  version          = "0.138.1"
+  version          = "0.140.0"
   namespace        = "splunk-otel-collector"
   create_namespace = true
 
